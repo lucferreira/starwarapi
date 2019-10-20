a25ca9bb5a132bed24f64f09fb10214be9322151
luciano-dsv <luciano-dsv@lucianodsv-Inspiron-5448>
2019-10-20 11:02:44 -0300

    [MODIFY] Modificação da classe para detectar pacotes do spring.

--- starwarapi/src/main/java/br/com/desafiob2w/starwarapi/StarwarapiApplication.java
+++ starwarapi/src/main/java/br/com/desafiob2w/starwarapi/StarwarapiApplication.java
@@ -2,8 +2,10 @@ package br.com.desafiob2w.starwarapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class StarwarapiApplication {
 
 	public static void main(String[] args) {
