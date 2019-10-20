f60b91531afee4a9ed0aa6eae9e08b765e6918ea
luciano-dsv <luciano-dsv@lucianodsv-Inspiron-5448>
2019-10-20 11:11:04 -0300

    [MODIFY] Revertendo alterações.

--- starwarapi/src/main/java/br/com/desafiob2w/starwarapi/StarwarapiApplication.java
+++ starwarapi/src/main/java/br/com/desafiob2w/starwarapi/StarwarapiApplication.java
@@ -2,10 +2,8 @@ package br.com.desafiob2w.starwarapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class StarwarapiApplication {
 
 	public static void main(String[] args) {
